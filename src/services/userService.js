cf1dc402763b32d5ca192fc812ec9987d752373c
Siddharth Sharma <92227248+ProSid2002@users.noreply.github.com>
2022-12-10 14:35:56 +0530

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://user-details-restapi.herokuapp.com/";
+axios.defaults.baseURL = "https://user-details-restapi.onrender.com";
 
 export async function getAllUsers() {
   try {
